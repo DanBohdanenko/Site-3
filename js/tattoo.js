9695fba6242bb1b1e7ea66ca84784f3d2f27965b
DanBohdanenko <109080690+DanBohdanenko@users.noreply.github.com>
2023-04-19 22:49:25 +0200

    Add files via upload

---
+++ js/tattoo.js
@@ -0,0 +1,30 @@
+//burger
+
+let burger = document.querySelector(".header__icon");
+let headerMenu = document.querySelector(".header__menu");
+burger.addEventListener("click", function (event) {
+  headerMenu.classList.toggle("_active");
+  burger.classList.toggle("_active");
+  document.body.classList.toggle("_lock");
+});
+let headerLinks = document.querySelectorAll(".header__link");
+for (let link of headerLinks) {
+  link.addEventListener("click", function (e) {
+    if (headerMenu.classList.contains("_active")) {
+      headerMenu.classList.toggle("_active");
+      document.body.classList.toggle("_lock");
+      burger.classList.toggle("_active");
+    }
+  });
+}
+
+//services
+let spoilerItems = document.querySelectorAll(".services__item");
+spoilerItems.forEach((item) => {
+  item.addEventListener("mousedown", function (e) {
+    e.preventDefault();
+  });
+  item.addEventListener("click", function (e) {
+    item.classList.toggle("_active");
+  });
+});
